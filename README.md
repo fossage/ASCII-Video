dbfa80a1753d219b2a34e384da02dd8bf41cb55c
Justin Foss <jfoss124@gmail.com>
2018-01-20 15:30:51 -0800

    Ensures frames are logged out as entire frames rather than line by line. Ensures program respects the --frame_rate flag

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # ASCII Video
 
-
 ## What is it
 
-ASCII Video is a command line tool which allows you to convert movies into ASCII sprite sheets and then play them back in the terminal. It uses [ffmpeg](https://ffmpeg.org/) to break up the video into a series of images, which then are converted to individual ASCII art frames using [image-to-ascii](https://github.com/IonicaBizau/image-to-ascii).  It then builds those frames up into a Javascript array as text, and writes the resulting array to an output file that can then be read in by the program and played back in the terminal with the help of [log-update](https://github.com/sindresorhus/log-update).
+ASCII Video is a command line tool which allows you to convert movies into ASCII sprite sheets and then play them back in the terminal. It uses [ffmpeg](https://ffmpeg.org/) to break up the video into a series of images, which then are converted to individual ASCII art frames using [image-to-ascii](https://github.com/IonicaBizau/image-to-ascii). It then builds those frames up into a Javascript array as text, and writes the resulting array to an output file that can then be read in by the program and played back in the terminal with the help of [log-update](https://github.com/sindresorhus/log-update).
 
-![ASCII Video is Magic](https://www.dropbox.com/s/f92h230quy9xeac/out.gif?dl=0&raw=true "Demo Video")
+![ASCII Video is Magic](https://www.dropbox.com/s/f92h230quy9xeac/out.gif?dl=0&raw=true 'Demo Video')
 
 ## Why would you make this
 
@@ -22,30 +21,33 @@ For me personally, I just use it to make things that make me laugh. If that is n
 
 ## How do I get it (installation)
 
-1. If you're on a Mac, the easiest way to get the initial dependencies is through [Homebrew](https://brew.sh/).  If you're not on a Mac then you'll have to figure out how to load the initial dependencies on your machine, but everything after installing Node and FFMPEG should be the same.
+1. If you're on a Mac, the easiest way to get the initial dependencies is through [Homebrew](https://brew.sh/). If you're not on a Mac then you'll have to figure out how to load the initial dependencies on your machine, but everything after installing Node and FFMPEG should be the same.
 
 1. Next you need to install [ffmpeg](https://ffmpeg.org/) by running:
-    ```bash
-    brew update && brew install ffmpeg
-    ```
+   ```bash
+   brew update && brew install ffmpeg
+   ```
 1. If you do not have [node](https://nodejs.org/en/), install it by running:
-    ```bash
-    brew update && brew install node
-    ```
+   ```bash
+   brew update && brew install node
+   ```
 1. Node comes with a package manager called NPM. To install [ASCII-Video](https://github.com/fossage/ASCII-Video) run:
-    ```bash
-    npm install ascii-video
-    ```
-    Alternatively, you can just simply clone the repo from github, and then install the projects dependencies by navigating to the cloned repo and running:
-    ```bash
-    npm install
-    ```
+
+   ```bash
+   npm install ascii-video
+   ```
+
+   Alternatively, you can just simply clone the repo from github, and then install the projects dependencies by navigating to the cloned repo and running:
+
+   ```bash
+   npm install
+   ```
 
 1. Next, navigate to the ASCII-Video directory within the node_modules directory and run:
-    ```bash
-    npm link
-    ```
-    This will allow you to run the program the same way you would any other bash program from anywhere on the machine.
+   ```bash
+   npm link
+   ```
+   This will allow you to run the program the same way you would any other bash program from anywhere on the machine.
 
 ## How do I use it
 
@@ -54,20 +56,20 @@ There are two commands that you use with `ascii-vido`; the `create` command and 
 ### The `create` command
 
 ```bash
-ascii-video create <[path/to/]input-video> <[path/to/]ouput-filename.js>
+ascii-video create <[path/to/]input-video> <[path/to/]ouput-filename.yaml>
 ```
 
 I.E.
 
 ```bash
-ascii-video create ~/Documents/foo.mov ~/Documents/ascii-video-output/foo.js
+ascii-video create ~/Documents/foo.mov ~/Documents/ascii-video-output/foo.yaml
 ```
 
-Both the input video and output filename are required.  Note that is should be able to convert most all video formats so there is no strict specification.  Both the input video and output filename can be preceeded with a relative or absolute path, but if any path is ommitted, then the program will assume you are referring to the current working directory.
+Both the input video and output filename are required. Note that is should be able to convert most all video formats so there is no strict specification. Both the input video and output filename can be preceeded with a relative or absolute path, but if any path is ommitted, then the program will assume you are referring to the current working directory.
 
-One other thing to note is that the ouput filename MUST be a Javascript file.
+One other thing to note is that the ouput filename MUST be a YAML file(ends with the .yaml extension).
 
-Lastly, note that currently the way the program will figure out what size to make the video is by determining the size of your terminal window when the command is run.  If you would like a larger, more detailed video, simply make your terminal window larger and vice-versa.
+Lastly, note that currently the way the program will figure out what size to make the video is by determining the size of your terminal window when the command is run. If you would like a larger, more detailed video, simply make your terminal window larger and vice-versa.
 
 ### The `play` command
 
@@ -81,7 +83,7 @@ I.E.
 ascii-video play ~/Documents/ascii-video-output/foo.js --frame_rate 20
 ```
 
-This command will play back a Javascript file that was created with the `create` command. You will likely want to use the `--frame_rate` flag, or just `-f` for shorthand, because I have found that different videos seem to vary pretty widely on what seems like an acceptable framerate.  It is defaulted to 155, but I will often find myself running it as low as 5 for some videos and as much as 200 for others.  Just play around until you find a setting for your particular video that works.
+This command will play back a YAML file that was created with the `create` command. You will likely want to use the `--frame_rate` flag, or just `-f` for shorthand, because I have found that different videos seem to vary pretty widely on what seems like an acceptable framerate. It is defaulted to 155, but I will often find myself running it as low as 5 for some videos and as much as 200 for others. Just play around until you find a setting for your particular video that works.
 
 Also, as mentioned in the create section, the size of the video is determined by the size of your terminal window at the time when you run the `create` command, so to ensure that it plays back in the correctly sized viewport, it is best to run `play` in the same terminal window(or at least on of the same size/aspect ratio) as the one in which the input file was created.
 
@@ -91,8 +93,4 @@ Also, as mentioned in the create section, the size of the video is determined by
 * Add flag to `create` to allow user to turn color encoding on/off
 * Add flag to `play` command to toggle looping on/off
 
-
-If you have any other improvements you would like to see, please feel free to submit a feature request.  Also, if you make any neat projects using this, please let me know so I can feature them on the README.  Thanks!
-
-
-
+If you have any other improvements you would like to see, please feel free to submit a feature request. Also, if you make any neat projects using this, please let me know so I can feature them on the README. Thanks!
--- main.js
+++ main.js
@@ -3,155 +3,191 @@
 /*=====================================================
                     IMPORTS / SETUP
 ======================================================*/
-const _ = require('lodash');
-const fs = require('fs');
-const yaml = require('js-yaml');
-const clc = require('cli-color');
-const shell = require('shelljs');
-const program = require('commander');
-const logUpdate = require('log-update');
-const imageToAscii = require("image-to-ascii");
+const _ = require('lodash')
+const fs = require('fs')
+const yaml = require('js-yaml')
+const clc = require('cli-color')
+const shell = require('shelljs')
+const program = require('commander')
+const logUpdate = require('log-update')
+const imageToAscii = require('image-to-ascii')
 
-const tmpDirPath = '/tmp/__sprite_cli_output/';
-
+const TMP_DIR_PATH = '/tmp/__sprite_cli_output/'
+const END_OF_FRAME_ID = '\nzzzzzzzzzzzzzzzzzzzzzzz'
 /*=====================================================
                           MAIN
 ======================================================*/
 program
-    .version('0.0.5')
-    .command('create <input-video> <output-filename>')
-    .description('Takes an input video, converts it into ASCII frames, and writes it to an output file.')
-    .action((video, outputTo) => {
-        if (!_.endsWith(outputTo, '.yaml')) {
-            return console.log(errMsg('The outputfile must be a yaml file.'));
+  .version('0.1.1')
+  .command('create <input-video> <output-filename>')
+  .description(
+    'Takes an input video, converts it into ASCII frames, and writes it to an output file.'
+  )
+  .action((video, outputTo) => {
+    if (!_.endsWith(outputTo, '.yaml')) {
+      return console.log(errMsg('The outputfile must be a yaml file.'))
+    }
+
+    // remove the output file if it exists already
+    if (fs.existsSync(outputTo)) {
+      fs.unlinkSync(outputTo)
+    }
+
+    const dir = process.cwd()
+    const finishLoadingId = showLoading()
+
+    // make temp directory to write image files to
+    shell.exec(`cd /tmp && mkdir __sprite_cli_output && cd ${dir}`)
+
+    if (
+      shell.exec(`ffmpeg -i ${program.args[0]} ${TMP_DIR_PATH}image%d.jpg`)
+        .code !== 0
+    ) {
+      // stop loading animation
+      clearInterval(finishLoadingId)
+      return console.log(errMsg('@todo: error message for shit went wrong.'))
+    }
+
+    // stop loading animation
+    clearInterval(finishLoadingId)
+
+    // ensure frames are in correct order
+    const files = fs
+      .readdirSync(TMP_DIR_PATH)
+      .sort((f1, f2) => {
+        try {
+          const fileOne = parseInt(f1.match(/\d/g).join(''))
+          const fileTwo = parseInt(f2.match(/\d/g).join(''))
+          return fileTwo - (fileOne + 1)
+        } catch (e) {
+          return 0
         }
+      })
+      .reverse()
 
-        // remove the output file if it exists already
-        if (fs.existsSync(outputTo)) {
-            fs.unlinkSync(outputTo);
-        }
-
-        const dir = process.cwd();
-        const finishLoadingId = showLoading();
-
-        // make temp directory to write image files to
-        shell.exec(`cd /tmp && mkdir __sprite_cli_output && cd ${dir}`);
-
-        if (shell.exec(`ffmpeg -i ${program.args[0]} ${tmpDirPath}image%d.jpg`).code !== 0) {
-            // stop loading animation
-            clearInterval(finishLoadingId);
-            return console.log(errMsg('@todo: error message for shit went wrong.'))
-        }
-
-        // stop loading animation
-        clearInterval(finishLoadingId);
-
-        // ensure frames are in correct order
-        const files = fs.readdirSync(tmpDirPath).sort((f1, f2) => {
-            try {
-                const fileOne = parseInt(f1.match(/\d/g).join(''));
-                const fileTwo = parseInt(f2.match(/\d/g).join(''));
-                return fileTwo - (fileOne + 1);
-            } catch (e) {
-                return 0
-            }
-        }).reverse();
-
-        createSprites(files, outputTo, 0, []);
-    });
+    createSprites(files, outputTo, 0, [])
+  })
 
 program
-    .command('play <file>')
-    .description('Plays back a generated sprite file')
-    .option('-f, --frame_rate <rate>', 'A number which specifies the rate at which to iterate through the sprites')
-    .action((pathToFile, opts) => {
-        console.log(pathToFile);
-        const lineReader = require('readline').createInterface({
-            input: require('fs').createReadStream(pathToFile)
-        });
+  .command('play <file>')
+  .description('Plays back a generated sprite file')
+  .option(
+    '-f, --frame_rate <rate>',
+    'A number which specifies the rate at which to iterate through the sprites'
+  )
+  .action((pathToFile, opts) => {
+    console.log(pathToFile)
+    const lineReader = require('readline').createInterface({
+      input: require('fs').createReadStream(pathToFile),
+    })
 
-        lineReader.on('line', line => {
-            const frame = yaml.safeLoad(line)[0];
-            const frameRate = (opts && opts.frame_rate) ? opts.frame_rate : 155;
+    let frame = ''
+    const re = RegExp(END_OF_FRAME_ID, 'g')
+    const frameRate = opts && opts.frame_rate ? opts.frame_rate : 155
 
-            setTimeout(() => logUpdate(frame), frameRate);
-        });
-    });
+    lineReader.on('line', async line => {
+      lineReader.pause()
+      const fragment = yaml.safeLoad(line)[0]
+      frame += fragment
 
-program.parse(process.argv);
-if (!program.args.length) program.help();
+      if (re.test(frame)) {
+        const frames = frame.split(END_OF_FRAME_ID)
+        for (let frameItem of frames) {
+          await delay(frameRate)
+          if (frameItem.length) logUpdate(frameItem)
+        }
+
+        frame = ''
+      }
+
+      lineReader.resume()
+    })
+  })
+
+program.parse(process.argv)
+if (!program.args.length) program.help()
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time))
+}
 
 /*=====================================================
                         HELPERS
 ======================================================*/
 function createSprites(files, outputTo, idx, sprites) {
-    if (idx === files.length) {
-        appendToFile(outputTo, sprites, true);
-        // clean up temp directory after the last chunk of sprites is written
-        shell.exec(`rm -rf ${tmpDirPath}`);
-        console.log(infoMsg(`File written to ${outputTo}`));
-    } else {
-        imageToAscii(tmpDirPath + files[idx], {
-            image_type: 'jpg'
-        }, (err, converted) => {
-            if (err) {
-                console.log(warningMsg(err));
-            } else {
-                sprites.push(converted);
+  if (idx === files.length) {
+    appendToFile(outputTo, sprites, true)
+    // clean up temp directory after the last chunk of sprites is written
+    shell.exec(`rm -rf ${TMP_DIR_PATH}`)
+    console.log(infoMsg(`File written to ${outputTo}`))
+  } else {
+    imageToAscii(
+      TMP_DIR_PATH + files[idx],
+      {
+        image_type: 'jpg',
+      },
+      (err, converted) => {
+        if (err) {
+          console.log(warningMsg(err))
+        } else {
+          sprites.push(converted + '\nzzzzzzzzzzzzzzzzzzzzzzz')
 
-                // write to disk before sprites array gets too large
-                if (sprites.length > 500) {
-                    appendToFile(outputTo, sprites);
-                    sprites = [];
-                }
+          // write to disk before sprites array gets too large
+          if (sprites.length > 500) {
+            appendToFile(outputTo, sprites)
+            sprites = []
+          }
 
-                logUpdate(`Creating sprites: ${Math.round((idx / files.length) * 100)}%`);
-            }
+          logUpdate(
+            `Creating sprites: ${Math.round(idx / files.length * 100)}%`
+          )
+        }
 
-            createSprites(files, outputTo, idx + 1, sprites);
-        });
-    }
+        createSprites(files, outputTo, idx + 1, sprites)
+      }
+    )
+  }
 }
 
 function appendToFile(outputTo, sprites) {
-    const outFile = yaml.safeDump(sprites);
+  const outFile = yaml.safeDump(sprites)
 
-    fs.appendFile(outputTo, outFile, (err) => {
-        if (err) return console.log(warningMsg(err));
-    });
+  fs.appendFile(outputTo, outFile, err => {
+    if (err) return console.log(warningMsg(err))
+  })
 }
 
 function showLoading() {
-    const frames = ['-', '\\', '|', '/'];
-    let i = 0;
+  const frames = ['-', '\\', '|', '/']
+  let i = 0
 
-    return setInterval(() => {
-        const frame = frames[i = ++i % frames.length];
-        logUpdate(`${frame} Converting video to frames ${frame}`);
-    }, 80)
+  return setInterval(() => {
+    const frame = frames[(i = ++i % frames.length)]
+    logUpdate(`${frame} Converting video to frames ${frame}`)
+  }, 80)
 }
 
 function readFile(pathTo) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(pathTo, (err, data) => {
-            if (!err) return resolve(data);
-            console.log(err);
-            reject(err);
-        })
-    });
+  return new Promise((resolve, reject) => {
+    fs.readFile(pathTo, (err, data) => {
+      if (!err) return resolve(data)
+      console.log(err)
+      reject(err)
+    })
+  })
 }
 
 function infoMsg(msg) {
-    const infoColor = clc.xterm(33);
-    return infoColor(msg);
+  const infoColor = clc.xterm(33)
+  return infoColor(msg)
 }
 
 function errMsg(msg) {
-    const errColor = clc.xterm(9);
-    return errColor(msg);
+  const errColor = clc.xterm(9)
+  return errColor(msg)
 }
 
 function warningMsg(msg) {
-    const warningColor = clc.xterm(214);
-    return warningColor(msg);
+  const warningColor = clc.xterm(214)
+  return warningColor(msg)
 }
